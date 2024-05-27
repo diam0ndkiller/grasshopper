ef79303d6d24195ada74204c9f46fa6acee81876
diam0ndkiller <>
2024-05-27 14:58:28 +0200

    Update Day One

---
+++ 03-vue-v-bind-01.js
@@ -0,0 +1,8 @@
+const image = Vue.createApp({
+    data() {
+        return {
+            url: "https://picsum.photos/400/400"
+        }
+    }
+})
+image.mount('#image')
---
+++ 03-vue-v-bind-02.js
@@ -0,0 +1,8 @@
+const style_app = Vue.createApp({
+    data() {
+        return {
+            size: "28px"
+        }
+    }
+})
+style_app.mount('#style_app')
