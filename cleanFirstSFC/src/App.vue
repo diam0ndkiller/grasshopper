4ca6b0f9bb11e38f56e48b072835481c0e8077cd
diam0ndkiller <>
2024-05-28 11:46:15 +0200

    commit custom cleanFirstSFC

---
+++ cleanFirstSFC/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+    <HelloWorld/>
+    <p>{{ text }}</p>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            text: 'This is Vue Text!'
+        }
+    }
+}
+</script>
+
+<style>
+</style>
---
+++ cleanFirstSFC/src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="helloworld">
+        <h2>Hello, World!</h2>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style>
+.helloworld {
+    font-family: Arial, Helvetica, sans-serif
+}
+</style>
